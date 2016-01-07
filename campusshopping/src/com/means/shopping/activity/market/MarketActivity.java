16ee2544a5203b1fc1dbff05315ad42858df8137
hu845643018 <845643018@qq.com>
2016-01-07 15:48:53 +0800

    更新商品详情

--- campusshopping/src/com/means/shopping/activity/market/MarketActivity.java
+++ campusshopping/src/com/means/shopping/activity/market/MarketActivity.java
@@ -1,17 +1,23 @@
 package com.means.shopping.activity.market;
 
+import net.duohuo.dhroid.net.JSONUtil;
+
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 import com.means.shopping.R;
 import com.means.shopping.adapter.CatAdapter;
 import com.means.shopping.adapter.HomePageAdapter;
 import com.means.shopping.api.API;
 import com.means.shopping.base.ShopBaseActivity;
+import com.means.shopping.bean.Good;
 import com.means.shopping.views.RefreshListViewAndMore;
+import com.means.shopping.views.dialog.CommodityDetailDialog;
 
 public class MarketActivity extends ShopBaseActivity {
 
@@ -50,6 +56,12 @@ public class MarketActivity extends ShopBaseActivity {
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1,
 					int position, long arg3) {
+				JSONObject jo = goodAdater.getTItem(position);
+				Long goodId = JSONUtil.getLong(jo, "area_id");
+				Good good = new Good();
+				good.setGoodId(goodId);
+				CommodityDetailDialog dialog = new CommodityDetailDialog(self,good);
+				dialog.show();
 
 			}
 		});
