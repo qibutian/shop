3659bd01301e31289a0e0a510384282558522058
hu845643018 <845643018@qq.com>
2016-01-11 16:10:49 +0800

    up

---
+++ campusshopping/src/com/means/shopping/activity/pay/PaymentActivity.java
@@ -0,0 +1,18 @@
+package com.means.shopping.activity.pay;
+
+import com.means.shopping.R;
+import com.means.shopping.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PaymentActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_payment);
+	}
+}
