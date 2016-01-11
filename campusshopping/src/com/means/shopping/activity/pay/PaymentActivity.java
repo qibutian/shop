df21e2a39fd094d7310d5483792798bd00c14734
hu845643018 <845643018@qq.com>
2016-01-11 16:59:37 +0800

    up

--- campusshopping/src/com/means/shopping/activity/pay/PaymentActivity.java
+++ campusshopping/src/com/means/shopping/activity/pay/PaymentActivity.java
@@ -2,17 +2,29 @@ package com.means.shopping.activity.pay;
 
 import com.means.shopping.R;
 import com.means.shopping.R.layout;
+import com.means.shopping.base.ShopBaseActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class PaymentActivity extends Activity {
+/**
+ * 付款页
+ * @author Administrator
+ *
+ */
+public class PaymentActivity extends ShopBaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_payment);
+	}
+
+	@Override
+	public void initView() {
+		// TODO Auto-generated method stub
+		
 	}
 }
