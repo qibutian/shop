c67db517f522e947282ea9844cd6398e18730594
qibutian <603986018@qq.com>
2016-01-21 15:50:41 +0800

    t

--- campusshopping/src/com/means/shopping/api/API.java
+++ campusshopping/src/com/means/shopping/api/API.java
@@ -3,5 +3,8 @@ package com.means.shopping.api;
 public class API {
 
 	public static String test = "http://www.foodies.im/wap.php?g=Wap&c=Travel&a=cityList";
+	
+	
+	public  static String marketCat = "";
 
 }
